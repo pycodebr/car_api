9d1864e24dbee59d04e70d820e988a7cc5427124
pycodebr <pycodebr@gmail.com>
2025-06-12 11:50:04 -0300

    add env var settings config file

--- car_api/database.py
+++ car_api/database.py
@@ -1,33 +1,8 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-
-# DATABASE_URL = 'sqlite:///./cars.db'
-
-# engine = create_engine(
-#     DATABASE_URL,
-#     connect_args={'check_same_thread': False},
-# )
-
-# SessionLocal = sessionmaker(
-#     autocommit=False,
-#     autoflush=False,
-#     bind=engine,
-# )
-
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-DATABASE_URL = 'sqlite+aiosqlite:///./cars.db'
+from car_api.settings import Settings
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(Settings().DATABASE_URL)
 
 
 async def get_session():
